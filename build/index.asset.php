fbd5bbc159aa3a56ed99046f1a147b8baaead799
sandip <sandip.posimyth@gmail.com>
2025-05-06 11:01:14 +0530

    dublect options issue sloved

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '1f1cb221269f89833825');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '7b95edce9d0a204d6bf8');
