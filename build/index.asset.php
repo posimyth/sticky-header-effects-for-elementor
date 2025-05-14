fc67106c71a1881849ee6248884709a7c4c81c8a
sandip <sandip.posimyth@gmail.com>
2025-05-14 11:15:41 +0530

    add Image

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '2bc7803e1d2291a108ec');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '7714a800a1d7400e2378');
