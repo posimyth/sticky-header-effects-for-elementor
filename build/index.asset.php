b94ca2e1d838b67efcb862702ebf0759aa27ca3d
sandip <sandip.posimyth@gmail.com>
2025-05-27 10:57:53 +0530

    Design Releted Bug Fix

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-i18n'), 'version' => 'a9c0cf28de9c999d3ae1');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-i18n'), 'version' => '9fd4fd3af88aa06fa868');
