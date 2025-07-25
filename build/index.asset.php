647a5fc8ad71c48caa1f37fc1e567d9b0cc12a46
sandip <sandip.posimyth@gmail.com>
2025-07-25 17:20:35 +0530

    update design issue

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '02390f74ab175a52b968');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '2cf628e9c069b2b0c422');
