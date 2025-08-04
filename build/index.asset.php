2a41b9b4d85ed02e5f02541224ba19773bb38ccd
sandip <sandip.posimyth@gmail.com>
2025-08-04 10:19:03 +0530

    tpae button chnage form theplus addon

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '0f75aecee6ee2f115fb4');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => 'e167cac47ec5fe06015d');
