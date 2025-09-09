6e270465422ab4bdf8cd1eb245d503e4245d0a64
sandip <sandip.posimyth@gmail.com>
2025-09-09 18:24:19 +0530

    Fix : Design Releted Bug Fix Form QA Side

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => 'c87c796346e850193171');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => 'a4c7cb32e248ea5e2182');
