1310809e04e297f6022c35ebf520a8e8ffda75f0
sandip <sandip.posimyth@gmail.com>
2025-11-19 11:37:58 +0530

    Update : WDKIT Link Update

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-i18n'), 'version' => '32f76875422c54d85bed');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-i18n'), 'version' => 'c10dcc456d272dda505e');
--- modules/nxt-ext/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-dom-ready', 'wp-i18n'), 'version' => 'f4bb88fff40e21ef5c6d');
