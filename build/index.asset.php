1b3fd20ae6fcf2b7bcbaaf30fc0331e895459ca7
Parth Padhiyar <parth.posimyth@gmail.com>
2026-06-04 13:21:01 +0530

    Dashboard : compiled assets for Activate License tab

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-i18n'), 'version' => '1a58daae169306209160');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-i18n'), 'version' => '267ea396cb4510c219ae');
